859d46867877ef27da6bf9191e81ee944609bb19
eriab90 <ingenieraerika4.0@gmail.com>
2024-03-08 21:07:11 -0500

    prueba/cuenta

--- class/cuenta.php
+++ class/cuenta.php
@@ -2,5 +2,25 @@
 
 class Cuenta
 {
+    private $nombre;
+    private $nrocuenta;
+    private $tipocuenta;
+    private $saldo;
+
+    public function __construct (
+        $nombre, $nrocuenta, $topocuenta, $saldo
+        )
+    {
+      $this->nombre = $nombre;
+      $this->nrocuenta = $nrocuenta;
+      $this->tipocuenta = $topocuenta;
+      $this->saldo = $saldo;  
+    }
     
+      public function setNombre($nombre)
+      {
+        $this->nombre = $nombre;
+      
+        
+    }
 }
